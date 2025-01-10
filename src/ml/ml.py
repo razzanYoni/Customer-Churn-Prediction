efd2931c189f75fb6c1816ba947a2759df6d4c8c
razzanYoni <razzanyoni@gmail.com>
2025-01-11 02:31:46 +0700

    refactor: add neural network model

--- src/ml/ml.py
+++ src/ml/ml.py
@@ -1,41 +1,44 @@
+import argparse
 import mlflow
+import mlflow.keras as keras
 import mlflow.sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, roc_auc_score
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
-import pandas as pd
 import numpy as np
-from datetime import datetime
+from pyspark.sql import SparkSession
+
+parser = argparse.ArgumentParser(description="Data Drift Monitoring and Model Retraining")
+parser.add_argument("--input_data_path", type=str, required=True, help="Path to the input dataset")
+parser.add_argument("--latest_model_path", type=str, required=True, help="Path to the model")
 
 mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.autolog()
 
-def preprocess_data(data):
-    """Preprocess the dataset by encoding categorical variables and scaling numerical features."""
-    # Encode categorical variables
-    data.drop(columns=['customerID'], inplace=True)
+# https://arize.com/blog-course/population-stability-index-psi/
+# https://github.com/mwburke/population-stability-index
+def calculate_psi(baseline, current, bins=10):
+    """Calculate Population Stability Index (PSI) for a single feature."""
+    baseline_bins = np.histogram(baseline, bins=bins)[0] / len(baseline)
+    current_bins = np.histogram(current, bins=bins)[0] / len(current)
 
-    categorical_columns = [
-        'gender', 'Partner', 'Dependents', 'PhoneService', 'MultipleLines', 'InternetService',
-        'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies',
-        'Contract', 'PaperlessBilling', 'PaymentMethod', 'Churn'
-    ]
+    # Avoid division by zero by adding a small value to bins
+    baseline_bins = np.clip(baseline_bins, 1e-6, None)
+    current_bins = np.clip(current_bins, 1e-6, None)
 
-    for col in categorical_columns:
-        data[col] = LabelEncoder().fit_transform(data[col])
+    psi = np.sum((baseline_bins - current_bins) * np.log(baseline_bins / current_bins))
+    return psi
 
-    # Scale numerical features
-    numerical_columns = ['MonthlyCharges', 'TotalCharges']
-    data[numerical_columns] = StandardScaler().fit_transform(data[numerical_columns])
+def calculate_psi_multiple_features(baseline, current, columns, bins=10):
+    """Calculate PSI for multiple features."""
+    psi_values = {}
+    for col in columns:
+        psi = calculate_psi(baseline[col], current[col], bins)
+        psi_values[col] = psi
+    return psi_values
 
-    target_column = [
-    'Churn'
-    ]
-    data['Churn'] = LabelEncoder().fit_transform(data['Churn'])
-
-    return data
-
-def train_initial_model(X_train, y_train, X_test, y_test):
+def train_model_logistic_regression(X_train, y_train, X_test, y_test):
     """Train the initial logistic regression model and log it to MLflow."""
     with mlflow.start_run(run_name="Initial_Model_Training"):
         model = LogisticRegression(random_state=42, max_iter=1000)
@@ -56,75 +59,71 @@ def train_initial_model(X_train, y_train, X_test, y_test):
 
         print(f"Model trained with accuracy: {accuracy}, ROC AUC: {roc_auc}")
 
-def calculate_psi(baseline, current, bins=10):
-    """Calculate Population Stability Index (PSI) for a given feature."""
-    baseline_bins = np.histogram(baseline, bins=bins)[0] / len(baseline)
-    current_bins = np.histogram(current, bins=bins)[0] / len(current)
+def train_model_neural_network(X_train, y_train, X_test, y_test, latest_model = None, isRetrain=False):
+    """Train the initial neural network model and log it to MLflow."""
+    with mlflow.start_run(run_name="Initial_Model_Training"):
+        # get weights from the latest model
+        model = keras.Sequential([
+            keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+            keras.layers.Dense(64, activation='relu'),
+            keras.layers.Dense(1, activation='sigmoid')
+        ])
 
-    psi = np.sum((baseline_bins - current_bins) * np.log(baseline_bins / current_bins))
-    return psi
+        if isRetrain:
+            model.set_weights(latest_model.get_weights())
+        model.compile(optimizer=keras.optimizers.Adam, loss=keras.losses.BinaryCrossentropy, metrics=['accuracy'])
 
-def monitor_drift_and_retrain(baseline, current, psi_threshold, X_train, y_train, X_test, y_test):
-    """Monitor drift using PSI and retrain the model if necessary."""
-    psi_value = calculate_psi(baseline, current)
-    print(f"Calculated PSI: {psi_value}")
+        model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
 
-    # Log PSI to MLflow
-    with mlflow.start_run(run_name="Drift_Monitoring"):
-        mlflow.log_metric("PSI", psi_value)
+        # Log model parameters
+        mlflow.log_param("model_type", "NeuralNetwork")
+        mlflow.log_param("epochs", 10)
 
-        if psi_value > psi_threshold:
-            print("Drift detected. Retraining the model...")
+        # Log model metrics
+        test_loss, test_accuracy = model.evaluate(X_test, y_test)
+        mlflow.log_metric("test_loss", test_loss)
+        mlflow.log_metric("test_accuracy", test_accuracy)
 
-            # Retrain the model
-            with mlflow.start_run(run_name="Model_Retraining"):
-                model = LogisticRegression(random_state=42, max_iter=1000)
-                model.fit(X_train, y_train)
+        # Log the model
+        mlflow.keras.log_model(model, "model", registered_model_name="CustomerChurnModel")
 
-                # Log retrained model parameters
-                mlflow.log_param("model_type", "LogisticRegression")
-                mlflow.log_param("max_iter", 1000)
-
-                # Log retrained model metrics
-                accuracy = accuracy_score(y_test, model.predict(X_test))
-                roc_auc = roc_auc_score(y_test, model.predict_proba(X_test)[:, 1])
-                mlflow.log_metric("accuracy", accuracy)
-                mlflow.log_metric("roc_auc", roc_auc)
-
-                # Log the retrained model
-                mlflow.sklearn.log_model(model, "model", registered_model_name="CustomerChurnModel")
-
-                print(f"Retrained model logged with accuracy: {accuracy}, ROC AUC: {roc_auc}")
+        if isRetrain:
+            print(f"Retrained model logged with test loss: {test_loss}, test accuracy: {test_accuracy}")
         else:
-            print("No significant drift detected. No retraining required.")
+            print(f"Model trained with test loss: {test_loss}, test accuracy: {test_accuracy}")
 
-def analyze_correlation(data):
-    """Analyze and print correlation between features."""
-    correlation_matrix = data.corr()
-    print("Feature Correlation Matrix:\n", correlation_matrix)
-    return correlation_matrix
-
-# Example usage
-if __name__ == "__main__":
-    # Load data from CSV
-    data = pd.read_csv("./data/raw/manual-delete-data.csv")
-
-    # Preprocess data
-    data = preprocess_data(data)
-    # Analyze correlation
-    analyze_correlation(data)
+# https://stackoverflow.com/questions/70111193/how-can-i-load-the-latest-model-version-from-mlflow-model-registry
+def monitor_drift_and_retrain(baseline, current, input_data_path, latest_model_path, psi_threshold = 0.1):
+    spark = SparkSession.builder.appName("Train Model").getOrCreate()
+    df = spark.read.parquet(input_data_path, header=True, inferSchema=True)
 
     # Split into features and target
-    X = data.drop(columns=['Churn'])
-    y = data['Churn']
+    X = df.drop(columns=['Churn'])
+    y = df['Churn']
 
     # Split data into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=123)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=488)
 
-    # Train initial model
-    train_initial_model(X_train, y_train, X_test, y_test)
+    # calculate PSI for only the numerical columns
+    columns = ['MonthlyCharges', 'TotalCharges']
+    psi_values = calculate_psi_multiple_features(baseline, current, columns)
+    # max of psi_values
+    psi_value = np.max(list(psi_values.values()))
+    print(f"Calculated PSI: {psi_value}")
 
-    # Monitor drift and retrain if necessary
-    monitor_drift_and_retrain(X_train['MonthlyCharges'], X_test['MonthlyCharges'], psi_threshold=0.1, 
-                              X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
+    if psi_value > psi_threshold:
+        print("Drift detected. Retraining the model...")
+        latest_model = keras.models.load_model(latest_model_path)
+        # Retrain the model
+        with mlflow.start_run(run_name="Model_Retraining"):
+            # Neural network model
+            train_model_neural_network(
+                X_train=X_train,
+                y_train=y_train,
+                X_test=X_test,
+                y_test=y_test,
+                latest_model=latest_model,
+                isRetrain=True
+            )
+    else:
+        print("No significant drift detected. No retraining required.")
